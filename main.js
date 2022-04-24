23170725aed2f35f955b904ac27b51a469625073
Victoria John <vicjohn241@gmail.com>
2022-04-24 17:37:11 +0100

    set up folder structure

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("yes, linked javascript correctly")
+
+2 + 2
+3 + 5
+
+
+
+function addTwoNumbers(numberOne, numberTwo) {
+    return numberOne + numberTwo;
+}
+
+
+// arguments
+
+addTwoNumbers(2, 3)
