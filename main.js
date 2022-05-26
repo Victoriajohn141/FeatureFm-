9267062cfdb63e1a3c6c90566f9415a8e6523ac0
Victoria John <vicjohn241@gmail.com>
2022-05-26 12:43:39 +0100

    dynamically created streaming platform
     and appended the list to ul tag

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+"use strict"
+
 console.log("yes, linked javascript correctly")
 
 2 + 2
@@ -17,7 +19,7 @@ addTwoNumbers(2, 3)
 function getSongInformation() {
     console.log('Calling function getSOngInformation')
     return {
-        artist: 'for King & COuntry',
+        artist: 'for King & Country',
         songName: 'Run',
         coverArt: 'https://res.cloudinary.com/feature-fm/image/fetch/s--IS6xrwXV--/w_424,h_424,c_lfill/c_scale,fl_relative,w_1.1/e_blur_region:800/f_auto/https%3A%2F%2Fimagestore.ffm.to%2Flink%2Fd27d55b5e180206564f6ab43fa728f54.png',
         streamingPlatforms: [
@@ -58,7 +60,20 @@ function getSongInformation() {
 
         const img = document.createElement('img')
         img.src = "https://res.cloudinary.com/feature-fm/image/fetch/s--RduBEAtU--/h_64,c_scale/f_auto/https%3A%2F%2Fassets.ffm.to%2Fimages%2Flogo%2Fmusic-service_applemusic_listen.png"
-        
-        
+        img.className = "tube";
+        const a = document.createElement('a')
+        a.className = "btn";
+        a.target = "_blank";
+        a.href = "https://www.youtube.com/watch?v=xxpDAU-ig_Y";
+        a.innerText = "Play";
+
+        list.appendChild(img);
+        list.appendChild(a);
+        let ul = document.querySelector('ul#streaming-platforms');
+
+        ul.appendChild(list);
+
+        console.log(a)
+        console.log(ul)
     } 
 )()
