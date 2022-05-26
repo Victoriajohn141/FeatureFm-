8bba2ec494949401aca47aec884d593d66c95323
Victoria John <vicjohn241@gmail.com>
2022-05-26 15:18:28 +0100

    added another streaming platform reduced profile container and image width to 350.

--- main.js
+++ main.js
@@ -61,6 +61,7 @@ function getSongInformation() {
         const img = document.createElement('img')
         img.src = "https://res.cloudinary.com/feature-fm/image/fetch/s--RduBEAtU--/h_64,c_scale/f_auto/https%3A%2F%2Fassets.ffm.to%2Fimages%2Flogo%2Fmusic-service_applemusic_listen.png"
         img.className = "tube";
+
         const a = document.createElement('a')
         a.className = "btn";
         a.target = "_blank";
@@ -75,5 +76,67 @@ function getSongInformation() {
 
         console.log(a)
         console.log(ul)
-    } 
+
+ 
+        const list2 = document.createElement('li');
+        list2.className = "streaming-platform-1";
+        console.log(list2)
+
+        const img2 = document.createElement('img')
+        img2.src = "https://cloudinary-cdn.ffm.to/s--e_GXTT_B--/h_64,c_scale/f_webp/https%3A%2F%2Fassets.ffm.to%2Fimages%2Flogo%2Fmusic-service_spotify.png";
+        img2.className ="tube";
+         
+        const a2 = document.createElement('a')
+        a2.href= "https://open.spotify.com/album/0xATpwMQQqS5Gb7gS16MIa";
+        a2.className = "btn";
+        a2.target = "_blank";
+        a2.innerText = "Play";
+
+        list2.appendChild(img2);
+        list2.appendChild(a2);
+
+        ul.appendChild(list2);
+
+        console.log(ul)
+
+
+        const list3 = document.createElement('li');
+        list3.className = "streaming-platform-1";
+        console.log(list3)
+
+        const img3 = document.createElement('img')
+        img3.src = "https://cloudinary-cdn.ffm.to/s--JovdmVc_--/h_64,c_scale/f_webp/https%3A%2F%2Fassets.ffm.to%2Fimages%2Flogo%2Fmusic-service_boomplay_new.png";
+        img3.className = "tube";
+
+        const a3 = document.createElement('a')
+        a3.className = "btn";
+        a3.href = "https://www.boomplay.com/share/album/40140729?srModel=COPYLINK&srList=IOS&ffm=FFM_97fc9427eddbf0bd7d40bbd42ac991ad";
+        a3.target = "_blank";
+        a3.innerText = "Play";
+         
+        list3.appendChild(img3);
+        list3.appendChild(a3);
+
+        ul.appendChild(list3);
+
+
+        const list4 = document.createElement('li');
+        list4.className = "streaming-platform-1";
+        console.log(list4)
+
+        const img4 = document.createElement('img');
+        img4.src = "https://cloudinary-cdn.ffm.to/s--afUcMju---/h_64,c_scale/f_webp/https%3A%2F%2Fassets.ffm.to%2Fimages%2Flogo%2Fmusic-service_audiomack.png";
+        img4.className = "tube";
+
+        const a4 = document.createElement('a');
+        a4.className = "btn";
+        a4.href = "https://audiomack.com/tome/album/lov?ffm=FFM_891fa8c99bdcd5ce0e1da0606d807595";
+        a4.target = "_blank";
+        a4.innerText = "play";
+
+        list4.appendChild(img4);
+        list4.appendChild(a4);
+
+        ul.appendChild(list4);
+    }
 )()
